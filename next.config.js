9b3fe3243b65a1f80203b621aaf7e3961bf972d1
Artyom Loyko <31865743+ArtyomLoyko@users.noreply.github.com>
2023-06-28 21:48:40 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_CURRENCY_API_KEY: process.env.NEXT_PUBLIC_CURRENCY_API_KEY,
-    NEXT_PUBLIC_CURRENCY_API: process.env.NEXT_PUBLIC_CURRENCY_API
-  },
-
- // Add basePath
-  basePath: '/github-pages',
-
+  basePath: '/github-pages'
 }
 
 module.exports = nextConfig
